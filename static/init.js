4a38bebb40edfb4b93f48f48b9ea1c9830ea83e5
Kenan Sulayman <kenan@sly.mn>
2015-09-18 23:09:39 +0200

    Fix indent

--- static/init.js
+++ static/init.js
@@ -3,168 +3,168 @@
 var events = require("./events.js");
 
 class TerminalLine {
-    constructor (message) {
-        this.element = document.createElement('line');
+	constructor (message) {
+		this.element = document.createElement('line');
 
-        this.setMessage(message);
-    }
+		this.setMessage(message);
+	}
 
-    setMessage (message) {
-        this.element.innerHTML = message;
-    }
+	setMessage (message) {
+		this.element.innerHTML = message;
+	}
 
-    getElement () {
-        return this.element;
-    }
+	getElement () {
+		return this.element;
+	}
 };
 
 class TerminalCommandLine {
-    constructor () {
-        this.element = document.createElement('inputline');
-        this.prompt = document.createElement('prompt');
-        this.input = document.createElement('input');
-        
-        this.input.className = 'command';
+	constructor () {
+		this.element = document.createElement('inputline');
+		this.prompt = document.createElement('prompt');
+		this.input = document.createElement('input');
+		
+		this.input.className = 'command';
 
-        this.element.appendChild(this.prompt);
-        this.element.appendChild(this.input);
-    }
+		this.element.appendChild(this.prompt);
+		this.element.appendChild(this.input);
+	}
 
-    setDisabled (disabled) {
-        this.input.setAttribute('disabled', disabled);
-    }
+	setDisabled (disabled) {
+		this.input.setAttribute('disabled', disabled);
+	}
 
-    setAutofocus () {
-        this.input.setAttribute('autofocus', true);
-    }
+	setAutofocus () {
+		this.input.setAttribute('autofocus', true);
+	}
 
-    setInput (message) {
-        this.input.value = message;
-    }
+	setInput (message) {
+		this.input.value = message;
+	}
 
-    setPrompt (prompt) {
-        this.prompt.innerText = prompt;
-    }
+	setPrompt (prompt) {
+		this.prompt.innerText = prompt;
+	}
 
-    getElement () {
-        return this.element;
-    }
+	getElement () {
+		return this.element;
+	}
 };
 
 class TerminalLineFeed {
-    constructor (output) {
-        this.lineFeed = [];
-        this.output = output;
-    }
+	constructor (output) {
+		this.lineFeed = [];
+		this.output = output;
+	}
 
-    push (line) {
-        this.output.appendChild(line.getElement());
-        this.lineFeed.push(line);
-    }
+	push (line) {
+		this.output.appendChild(line.getElement());
+		this.lineFeed.push(line);
+	}
 
-    remove (line) {
-        this.output.removeChild(line.getElement());
+	remove (line) {
+		this.output.removeChild(line.getElement());
 
-        let itemPos = this.lineFeed.indexOf(line);
+		let itemPos = this.lineFeed.indexOf(line);
 
-        ~itemPos && this.lineFeed.splice(itemPos, 1);
-    }
+		~itemPos && this.lineFeed.splice(itemPos, 1);
+	}
 }
 
 class Terminal extends events {
 	constructor () {
-        super();
+		super();
 
 		this.init();
 
-        this.registerEvents();
+		this.registerEvents();
 	}
 
 	init () {
-        this._promptPrefix = 'nobody@apx:~$';
+		this._promptPrefix = 'nobody@apx:~$';
 
 		this.container = this._initContainer();
 
 		this.command = this.container.querySelector('inputline .command');
 		this.output = this.container.querySelector('output');
 
-        this.lineFeed = new TerminalLineFeed(this.output);
+		this.lineFeed = new TerminalLineFeed(this.output);
 	}
 
-    _inputLine (readOnly) {
-        return new TerminalCommandLine ();
-    }
+	_inputLine (readOnly) {
+		return new TerminalCommandLine ();
+	}
 
 	_initContainer () {
 		let container = document.createElement('cream');
 		container.className = 'box';
 
-        this.output = document.createElement('output');
+		this.output = document.createElement('output');
 
-        this.inputLine = this._inputLine();
-        
-        this.inputLine.setPrompt(this._promptPrefix);
-        this.inputLine.setAutofocus(true);
+		this.inputLine = this._inputLine();
+
+		this.inputLine.setPrompt(this._promptPrefix);
+		this.inputLine.setAutofocus(true);
 
 		document.body.appendChild(container);
 
 		container.appendChild(this.output);
-        container.appendChild(this.inputLine.getElement());
+		container.appendChild(this.inputLine.getElement());
 
 		return container;
 	}
 
-    registerEvents() {
-        this.command.addEventListener('keydown', (e) => {
-            if (e.keyCode === 13) {
-                this.command.disabled = true;
-                this.emit('command', this.command.value);
-            }
-        });
-    }
+	registerEvents() {
+		this.command.addEventListener('keydown', (e) => {
+			if (e.keyCode === 13) {
+				this.command.disabled = true;
+				this.emit('command', this.command.value);
+			}
+		});
+	}
 
-    // write partial message, non-exit
-    write (msg) {
-        msg = msg.split('\n').map((line) => line.trim());
-        msg = msg.map((line) => {
-            return new TerminalLine(line);
-        });
+	// write partial message, non-exit
+	write (msg) {
+		msg = msg.split('\n').map((line) => line.trim());
+		msg = msg.map((line) => {
+			return new TerminalLine(line);
+		});
 
-        let fakeInputLine = new TerminalCommandLine();
+		let fakeInputLine = new TerminalCommandLine();
 
-        fakeInputLine.setDisabled(true);
-        fakeInputLine.setInput(this.command.value);
-        fakeInputLine.setPrompt(this._promptPrefix);
+		fakeInputLine.setDisabled(true);
+		fakeInputLine.setInput(this.command.value);
+		fakeInputLine.setPrompt(this._promptPrefix);
 
-        this.lineFeed.push(fakeInputLine);
+		this.lineFeed.push(fakeInputLine);
 
-        msg.forEach((line) => {
-            this.lineFeed.push(line);
-        });
-    }
+		msg.forEach((line) => {
+			this.lineFeed.push(line);
+		});
+	}
 
-    // quit command
-    commit (msg) {
-        this.command.value = '';
-        this.command.disabled = false;
+	// quit command
+	commit (msg) {
+		this.command.value = '';
+		this.command.disabled = false;
 
-        this.command.focus();
-    }
+		this.command.focus();
+	}
 };
 
 class apx extends events {
-    constructor() {
-        super();
+	constructor() {
+		super();
 
-        this.terminal = new Terminal();
+		this.terminal = new Terminal();
 
-        this.terminal.on('command', (msg) => {
-            setTimeout(() => {
-                this.terminal.write('yolo');
-                this.terminal.commit();
-            }, 2000);
-        });
-    }
+		this.terminal.on('command', (msg) => {
+			setTimeout(() => {
+				this.terminal.write('yolo');
+				this.terminal.commit();
+			}, 2000);
+		});
+	}
 };
 
 new apx();
